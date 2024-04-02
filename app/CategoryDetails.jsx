46efd9af362c85a8e65cb5b7e836db136e64a98d
utk145 <utkarsh20241a0514@grietcollege.com>
2024-04-02 19:42:28 +0530

    feat: implemented CategoryInfo component to display detailed category information along with delete category functionality

--- app/CategoryDetails.jsx
+++ app/CategoryDetails.jsx
@@ -1,15 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { useLocalSearchParams } from 'expo-router'
+import { supabase } from '../utils/supabase.config';
+import { FontAwesome } from '@expo/vector-icons';
+import { router } from 'expo-router';
+import CategoryInfo from '../components/CategoryDetails/CategoryInfo';
+
 
 export default function CategoryDetails() {
 
     const { categoryId } = useLocalSearchParams();
-    console.log("categoryId is ", categoryId);
+    const [categoryData, setCategoryData] = useState();
+    
+    useEffect(() => {
+        console.log("categoryId is ", categoryId);
+        categoryId && getCategoryDetails();
+    }, [categoryId]);
+
+
+    const getCategoryDetails = async () => {
+        const { data } = await supabase
+            .from("Category")
+            .select('*,CategoryItem(*)')
+            .eq("id", categoryId);
+
+        console.log("Category: ", data);
+        setCategoryData(data[0]); // to retrive an object which is inside [] data
+    };
 
     return (
-        <View>
-            <Text>CategoryDetails</Text>
+        <View style={styles.mainView}>
+            <FontAwesome name="arrow-circle-left" size={35} color="black"
+                onPress={() => router.back()}
+            />
+
+            <CategoryInfo categoryData={categoryData} />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    mainView: {
+        padding: 20,
+        marginTop: 20
+    },
+})
